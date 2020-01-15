d141c531f6eef4bb15c09649962216549ea41bdb
courserajpdata <msjenpeterson@gmail.com>
2020-01-14 20:32:50 -0600

    remove python_code.py

--- python_code.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-print("Hello Capstone Project Course!")
-Hello Capstone Project Course!")
-Hello Capstone Project Course!")
-Hello Capstone Project Course!")
-Hello Capstone Project Course!")
-
